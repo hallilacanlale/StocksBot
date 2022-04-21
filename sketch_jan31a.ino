c665196b9fb505bf2087c124db65df9b92f2d029
Halli Lacanlale <57775171+hallilacanlale@users.noreply.github.com>
2022-04-21 09:26:09 -0500

    Update sketch_jan31a.ino

--- sketch_jan31a.ino
+++ sketch_jan31a.ino
@@ -1,30 +1,135 @@
-// 1- Sarah Athar - 653568210
-// 2- Lab 3 - Scrolling Output
-// 3- Display Namme and Quote on a 16X2 LCD screen and make sure that the quote has a scrolling output
-// 4- It is important to make sure the brightness is adjusted in order to see the output correctly
-// 5- Used Arduino's Demo Link provided in the lab, as starter code for the Lab
-// 6- Office Hours demonstration 2/8/2022,
-
-// include the library code:
 #include <LiquidCrystal.h>
+int datafromUser;
 
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
-const int rs = 12, en = 11, d4 = 2, d5 = 3, d6 = 4, d7 = 5;
+const int buzzer = 10;
+const int RedLED = 9;
+const int GreenLED = 8;
+
+int iByte = 6;
+
+const int rs = 11, en = 12, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+typedef struct {
+  String stock; //char can also be a fixed length string like char fruit[16];
+  int count;
+  float price;
+  int rsi[6] = {60,78,50,28,35,90};
+} Stock;
+
+
+typedef struct
+  {
+      int one;
+      int two;
+      int three;
+  }  record_type;
+
+Stock stock[3];
+
+record[2] = (record_type) {1,2,3}
+
+
+ledVals = [10, 20, 18, 15, 19, 20, 18];
+int i = 0;
+int prevVal = 0;
+
+
 void setup() {
-  // set up the LCD's number of columns and rows:
+  Serial.begin(9600);
+  pinMode(RedLED,OUTPUT);
+  pinMode(GreenLED,OUTPUT);
+  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an out
   lcd.begin(16, 2);
-  // Print a message to the LCD.   
+  lcd.setCursor(0, 1);
+  lcd.clear();
 }
 
+
+
+void notificationSystem(int iByte) {
+  if(iByte - '0' == 3)  {
+    lcd.clear();
+    lcd.print("BUY!!");
+    tone(buzzer, 1000); // Send 1KHz sound signal...
+    delay(1000);        // ...for 1 sec
+    noTone(buzzer);     // Stop sound...
+    delay(1000);        // ...for 1sec
+  }
+
+  else if(iByte - '0' == 4)  {
+    lcd.clear();
+    lcd.print("SELL!!");
+    tone(buzzer, 1000); // Send 1KHz sound signal...
+    delay(1000);        // ...for 1 sec
+    noTone(buzzer);     // Stop sound...
+    delay(1000);        // ...for 1sec
+  }
+  
+  
+}
+
+void ledSystem(int val) {
+  if(val == 1)  {
+      digitalWrite(RedLED, LOW);
+      lcd.clear();
+      lcd.print("AAPL went up");
+      digitalWrite( GreenLED , HIGH );
+      delay(800); 
+      digitalWrite(GreenLED, LOW);    // turn the LED off by making the voltage LOW
+      delay(800); 
+    }
+    else if(val == 0)  {
+      digitalWrite( GreenLED, LOW);
+      lcd.clear();
+      lcd.print("AAPL went down");
+      digitalWrite(RedLED, HIGH);
+      delay(800);                       // wait for a second
+      digitalWrite(RedLED, LOW);    // turn the LED off by making the voltage LOW
+      delay(800); 
+    }
+}
+
+const int length = 12;
+
 void loop() {
-  // set the cursor to column 0, line 1
-  lcd.print("Sarah Athar")
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  lcd.print("My favorite quote")
-  // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
+  static char message[length];
+  static int message_pos = 0;
+  while (Serial.available() > 0){
+    char inByte = Serial.read();
+    if (inByte != ';' && (message_pos < length -1) ) {
+      message[message_pos] = inByte;
+      message_pos++;    
+    } else {
+      message[message_pos] = '\0';
+      message_pos = 0;
+    }
+  }
+  Serial.print(message);
+
+
+  if (prevVal > ledVals[i]) { //red 
+      ledSystem(0);
+  } else { //green
+      ledSystem(1);
+  }
+  prevVal = ledVals[i];
+
+  if (i == ledVals.size()){
+    i = 0;
+  } else {
+    i++; 
+  }
+  delay(5);
+  
+//
+//  if (strcmp(message, "red") == 0) {
+//    lcd.clear();
+//    lcd.print("yo");
+  
+//    if(Serial.available() > 0)  {
+//      iByte = Serial.read();
+//    }
+//    ledSystem(iByte);
+//    notificationSystem(iByte);
 }
